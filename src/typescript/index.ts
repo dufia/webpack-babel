2d28e2b0115ffd270ced143f82560e9235079e6e
dufia <moskalkonrad@gmail.com>
2019-02-25 23:12:08 +0000

    added some additional ts

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,3 +1,35 @@
 const constant: string = 'string';
 
+interface ClockInterface {
+  tick();
+}
+
+interface ClockContructor {
+  new (hh: number, mm: number): ClockInterface;
+}
+
+class DigitalClock implements ClockInterface {
+  constructor(hh: number, mm: number) {}
+  tick() {
+    console.log('beep');
+  }
+}
+
+class AnalogClock implements ClockInterface {
+  constructor(hh: number, mm: number) {}
+  tick() {
+    console.log('tick');
+  }
+}
+
+function createClock (clock: ClockContructor, hh: number, ss: number): ClockInterface {
+  return new clock(hh, ss);
+}
+
+let digitalClock = createClock(DigitalClock, 22, 54);
+let analogClock = createClock(AnalogClock, 10, 10);
+
+digitalClock.tick();
+analogClock.tick();
+
 export default constant;
